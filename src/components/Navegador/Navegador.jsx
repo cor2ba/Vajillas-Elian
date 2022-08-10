156e5e294e9cbdfced008ce833730f431aca2e95
cor2ba <cordobagabrielignacio@gmail.com>
2022-08-10 03:38:42 -0300

    Menu responsive y estilos en menu

--- src/components/Navegador/Navegador.jsx
+++ src/components/Navegador/Navegador.jsx
@@ -1,43 +1,82 @@
 import React from "react";
 import Estilos from "./Navegador.module.css";
 import { Link } from "react-router-dom";
+import menuimg from "../../imagenes/hamburgermenu.png";
 
 const Navegador = ({ color }) => {
   return (
-    <div className={Estilos.Contenedor}>
-      <div className={Estilos.ContenedorDeMarca}>
-        <h1 className={Estilos.Logo}>V.E</h1>
-        <h1 className={Estilos.TextoElian}>Elian</h1>
+    <div>
+      <div className={Estilos.ContainerResponsive}>
+        <label for="menu" className={Estilos.MenuImg}>
+          <img className={Estilos.Img} src={menuimg} alt="Menu" />
+        </label>
+        <input type="checkbox" id="menu" className={Estilos.NavInput} />
+        <div className={Estilos.Menu}>
+          <Link className={Estilos.No} to="/">
+            {color === "inicio" ? (
+              <h1 className={Estilos.LinkColor}>INICIO</h1>
+            ) : (
+              <h1 className={Estilos.Link}>INICIO</h1>
+            )}
+          </Link>
+          <Link className={Estilos.No} to="/productos">
+            {color === "productos" ? (
+              <h1 className={Estilos.LinkColor}>PRODUCTO</h1>
+            ) : (
+              <h1 className={Estilos.Link}>CONTACTO</h1>
+            )}
+          </Link>
+          <Link className={Estilos.No} to="/contacto">
+            {color === "contacto" ? (
+              <h1 className={Estilos.LinkColor}>CONTACTO</h1>
+            ) : (
+              <h1 className={Estilos.Link}>CONTACTO</h1>
+            )}
+          </Link>
+          <Link className={Estilos.No} to="/dondeestamos">
+            {color === "dondeestamos" ? (
+              <h1 className={Estilos.LinkColor}>DONDE ESTAMOS</h1>
+            ) : (
+              <h1 className={Estilos.Link}>DONDE ESTAMOS</h1>
+            )}
+          </Link>
+        </div>
       </div>
-      <div className={Estilos.ContenedorDeLinks}>
-        <Link className={Estilos.No} to="/">
-          {color === "inicio" ? (
-            <h1 className={Estilos.LinkColor}>INICIO</h1>
-          ) : (
-            <h1 className={Estilos.Link}>INICIO</h1>
-          )}
-        </Link>
-        <Link className={Estilos.No} to="/productos">
-          {color === "productos" ? (
-            <h1 className={Estilos.LinkColor}>PRODUCTOS</h1>
-          ) : (
-            <h1 className={Estilos.Link}>PRODUCTOS</h1>
-          )}
-        </Link>
-        <Link className={Estilos.No} to="/contacto">
-          {color === "contacto" ? (
-            <h1 className={Estilos.LinkColor}>CONTACTO</h1>
-          ) : (
-            <h1 className={Estilos.Link}>CONTACTO</h1>
-          )}
-        </Link>
-        <Link className={Estilos.No} to="/dondeestamos">
-          {color === "dondeestamos" ? (
-            <h1 className={Estilos.LinkColor}>¿DONDE ESTAMOS?</h1>
-          ) : (
-            <h1 className={Estilos.Link}>¿DONDE ESTAMOS?</h1>
-          )}
-        </Link>
+      <div className={Estilos.Contenedor}>
+        <div className={Estilos.ContenedorDeMarca}>
+          <h1 className={Estilos.Logo}>V.E</h1>
+          <h1 className={Estilos.TextoElian}>Elian</h1>
+        </div>
+        <div className={Estilos.ContenedorDeLinks}>
+          <Link className={Estilos.No} to="/">
+            {color === "inicio" ? (
+              <h1 className={Estilos.LinkColor}>INICIO</h1>
+            ) : (
+              <h1 className={Estilos.Link}>INICIO</h1>
+            )}
+          </Link>
+          <Link className={Estilos.No} to="/productos">
+            {color === "productos" ? (
+              <h1 className={Estilos.LinkColor}>PRODUCTOS</h1>
+            ) : (
+              <h1 className={Estilos.Link}>PRODUCTOS</h1>
+            )}
+          </Link>
+          <Link className={Estilos.No} to="/contacto">
+            {color === "contacto" ? (
+              <h1 className={Estilos.LinkColor}>CONTACTO</h1>
+            ) : (
+              <h1 className={Estilos.Link}>CONTACTO</h1>
+            )}
+          </Link>
+          <Link className={Estilos.No} to="/dondeestamos">
+            {color === "dondeestamos" ? (
+              <h1 className={Estilos.LinkColor}>¿DONDE ESTAMOS?</h1>
+            ) : (
+              <h1 className={Estilos.Link}>¿DONDE ESTAMOS?</h1>
+            )}
+          </Link>
+        </div>
       </div>
     </div>
   );
